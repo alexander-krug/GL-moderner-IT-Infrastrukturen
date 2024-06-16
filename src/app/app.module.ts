2dc32ffeb19168792c30be4bc0ed726dc22425b2
Tobias Witte <witte@to-w.de>
2024-06-16 15:29:12 +0200

    *updated all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CoreModule } from './core.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    SharedModule,
-    CoreModule,
+@NgModule({ declarations: [
+        AppComponent,
+        HeaderComponent,
     ],
-  bootstrap: [AppComponent],
-})
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        SharedModule,
+        CoreModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {}
