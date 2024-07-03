2eeb77d2bfbf10a8ae3d565eb0703bdf7ee9de4f
gfrtill <121062835+gfrtill@users.noreply.github.com>
2024-07-03 11:46:00 +0200

    bererchnen funzt

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -5,6 +5,8 @@ import {ShoppingListService} from '../../shopping-list/shopping-list.service';
 import {RecipesService} from '../recipes.service';
 
 import {ActivatedRoute, Params, Router} from '@angular/router';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms'
 
 @Component({
   selector: 'app-recipe-detail',
@@ -15,12 +17,14 @@ export class RecipeDetailComponent implements OnInit {
   recipeToDisplay!:Recipe;
   id!: number;
   amount = 1;
+  ingredientsVorhanden = Array<Ingredient>()
 
   constructor(
               private slService: ShoppingListService,
               private recipesService: RecipesService,
               private route: ActivatedRoute,
               private router:Router,
+
               ){}
 
   ngOnInit(){
@@ -37,6 +41,12 @@ export class RecipeDetailComponent implements OnInit {
       var toAdd = JSON.parse(JSON.stringify(this.recipeToDisplay))
       for (var ingredientAdd of toAdd.ingredients){
         ingredientAdd.amount=ingredientAdd.amount*this.amount
+        
+        for (var ingredientVorhanden of this.ingredientsVorhanden){
+          if (ingredientAdd.name == ingredientVorhanden.name) {
+            ingredientAdd.amount=ingredientAdd.amount-ingredientVorhanden.amount
+          }
+        }
       }
       this.slService.addIngredients(toAdd.ingredients);
     }
@@ -47,17 +57,20 @@ export class RecipeDetailComponent implements OnInit {
     this.router.navigate(['../'], {relativeTo: this.route});
   }
   updateAmount(event: any){
-    this.amount=event.data
+    this.amount=event.target.value
   }
-  // updateVorhanden(event: any, name: string){
-  //   console.log(name)
-  //   for (let item of this.vorhanden) {
-  //     if (item.name==name){
-  //       item.value=event.data;
-  //     }
-  //     else {
-  //       this.vorhanden.push({"name": name, "value": event.data})
-  //     }
-  //   }
-  // }
+  updateVorhanden(event: any, name: string){
+    var vorhandenFound=false
+    for (let item of this.ingredientsVorhanden) {
+      if (item.name==name){
+        item.amount=event.target.value;
+        vorhandenFound=true
+      } 
+    }
+
+    if (vorhandenFound==false) {
+      this.ingredientsVorhanden.push({"name": name, "amount": event.target.value})
+    }
+    console.log(event.target.value)
+  }
 }
